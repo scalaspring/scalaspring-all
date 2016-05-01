03c9bd701fce947d09b15f4d45c36ecbe4b697b2
Lance Arlaus <lance.arlaus@gmail.com>
2016-05-01 18:23:34 -0400

    build wip

--- build.sbt
+++ build.sbt
@@ -1,8 +1,32 @@
+import ScalaSpringProject._
 
-scalaVersion        := "2.11.8"
-
-scalacOptions       ++= Seq("-feature", "-deprecation")
-
-parallelExecution := false
+//lazy val commonSettings = Seq(
+//  organization        := "com.github.scalaspring",
+//  scalaVersion        := "2.11.8",
+//  crossScalaVersions  := Seq("2.10.6"),
+//  //javacOptions        := Seq("-source", "1.7", "-target", "1.7"),
+//  scalacOptions       ++= Seq("-feature", "-deprecation"),
+//  // Publishing settings
+//  publishMavenStyle       := true,
+//  publishArtifact in Test := false,
+//  pomIncludeRepository := { _ => false },
+//  publishTo := {
+//    val nexus = "https://oss.sonatype.org/"
+//    if (isSnapshot.value)
+//      Some("snapshots" at nexus + "content/repositories/snapshots")
+//    else
+//      Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+//  }
+//
+//)
+//
+//
+//
+//
+//scalaVersion        := "2.11.8"
+//
+//scalacOptions       ++= Seq("-feature", "-deprecation")
+//
+//parallelExecution := false
 
 //aggregate in publishLocal := false
--- project/Build.scala
+++ project/Build.scala
@@ -1,19 +1,29 @@
 import sbt._
-import Keys._
+import ScalaSpringProject._
 
-object AllBuild extends Build {
+object Build extends sbt.Build {
 	  
 	lazy val `scalaspring-all` = project in file(".") aggregate(`scalatest-spring`, `akka-spring-boot`, `akka-http-spring-boot`)
-	`scalaspring-all`.settings(
-		publish := {},
-    scalacOptions       ++= Seq("-feature", "-deprecation")
-  )
 
-	lazy val `scalatest-spring` = project in file("scalatest-spring")
+	lazy val `scalatest-spring` = (project in file("scalatest-spring"))
+    .withScalaSpringSettings(
+      "scalatest-spring",
+      "Integrates ScalaTest with Spring to manage test context lifecycle using standard Spring annotations and a stackable Scala trait"
+    )
 
-	lazy val `akka-spring-boot` = project in file("akka-spring-boot") dependsOn (`scalatest-spring` % "test->compile")
+	lazy val `akka-spring-boot` = (project in file("akka-spring-boot"))
+    .dependsOn(`scalatest-spring` % "test->compile")
+    .withScalaSpringSettings(
+      "akka-spring-boot",
+      "Scala-based integration of Akka with Spring Boot.\nTwo-way Akka<->Spring configuration bindings and convention over configuration with sensible automatic defaults get your project running quickly."
+    )
 
-	lazy val `akka-http-spring-boot` = project in file("akka-http-spring-boot") dependsOn (`akka-spring-boot`, `scalatest-spring` % "test->compile")
+	lazy val `akka-http-spring-boot` = (project in file("akka-http-spring-boot"))
+    .dependsOn(`akka-spring-boot`, `scalatest-spring` % "test->compile")
+    .withScalaSpringSettings(
+      "akka-http-spring-boot",
+      "Integrates Scala Akka HTTP and Spring Boot for rapid, robust service development with minimal configuration.\nPre-configured server components require little more than a route to get a service up and running."
+    )
 
 //	lazy val `akka-http-spring-boot-activator` = project in file("akka-http-spring-boot-activator") dependsOn (`akka-http-spring-boot`, `scalatest-spring` % "test->compile")
 
